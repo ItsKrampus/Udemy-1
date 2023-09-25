04e67b00b9a659f314c2145d62f115a04412c850
Sandro Pirveli <106743990+ItsKrampus@users.noreply.github.com>
2023-09-25 23:15:21 +0400

    Update guess.js
    bug fix

--- guess.js
+++ guess.js
@@ -1,23 +1,27 @@
 let maximum = parseInt(prompt("Enter the maximum number!"));
-while (!maximum ){
+while (!maximum) {
   maximum = parseInt(prompt("Enter a valid number"));
 }
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 
-let guess=parseInt(prompt("Enter your first guess!"));
-let attempts=1
-while(parseInt(guess)!==targetNum){
-  if(guess==="q"){
-    break
-  }
-  attempts++;
-  if(guess>targetNum){
-    guess=prompt("Too high try again!");
-  } else{
-    guess=prompt("Too low try again!");
+let guess = prompt("Enter your first guess!");
+let attempts = 1;
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") {
+    break;
+  }guess=parseInt(guess)
+  if (guess > targetNum) {
+    guess = prompt("Too high try again!");
+    attempts++;
+  } else if(guess<targetNum) {
+    guess = prompt("Too low try again!");
+    attempts++;
+  }else{
+    guess=prompt("Invalid number. Please enter a number or 'q' to quit ")
   }
 }
-if(guess==="q"){
-  console.log("Ok you quit!")
+if (guess === "q") {
+  console.log("Ok you quit!");
+}else{
+console.log(`You got it! it took you ${attempts} guesses`);
 }
-console.log(`You got it! it took you ${attempts} guesses`) 
