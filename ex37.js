8265c537ed0d7f8a2cb8c8b1ee67251b2fd45e53
Sandro Pirveli <106743990+ItsKrampus@users.noreply.github.com>
2023-09-26 20:17:23 +0400

    Create ex37.js

---
+++ ex37.js
@@ -0,0 +1,9 @@
+// define isSnakeEyes below:
+function isSnakeEyes(inp1, inp2){
+    if(inp1===1 && inp2===1){
+        console.log("Snake Eyes!")
+    }
+    else{
+        console.log("Not Snake Eyes!")
+    }
+}
