f2d25078ffac16ea71298e7b53629e61152e4f49
Sandro Pirveli <106743990+ItsKrampus@users.noreply.github.com>
2023-10-01 23:13:23 +0400

    Create app.js

---
+++ ex55/app.js
@@ -0,0 +1,5 @@
+const container= document.querySelector('#container');
+container.style.textAlign="center";
+const tree=document.querySelector('img');
+tree.style.width="150px"
+tree.style.borderRadius="50%"
