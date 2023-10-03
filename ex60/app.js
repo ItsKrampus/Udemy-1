4504eea86055b392740f73f19f910da55d5449e3
Sandro Pirveli <106743990+ItsKrampus@users.noreply.github.com>
2023-10-03 18:28:06 +0400

    Create app.js

---
+++ ex60/app.js
@@ -0,0 +1,8 @@
+const hello=document.querySelector("#hello")
+const goodbye=document.querySelector("#goodbye")
+hello.addEventListener("clicked", ()=>{
+  console.log("hello")
+})
+goodbye.addEventListener("clicked",()=>{
+  console.log("goodbye")
+})
