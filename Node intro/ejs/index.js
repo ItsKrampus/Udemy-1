1b50c3340fb0da2fa5e46d422b4f8e8c9a578869
Sandro Pirveli <106743990+ItsKrampus@users.noreply.github.com>
2023-10-20 20:30:12 +0400

    Update index.js

--- Node intro/ejs/index.js
+++ Node intro/ejs/index.js
@@ -11,6 +11,14 @@ app.get("/", (req, res)=>{
     res.render('home')
 })
 
+app.get("/rand",(req, res)=>{
+    const num= Math.floor(Math.random() * 10 )+1;
+    res.render('random',{num})
+})
+
+
+
+
 app.listen(3000, ()=> {
     console.log("Kaka");
 })
