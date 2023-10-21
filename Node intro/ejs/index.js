ca7300fc71a5734414fe41c16de930fa7d365066
Sandro Pirveli <106743990+ItsKrampus@users.noreply.github.com>
2023-10-21 13:56:32 +0400

    Update index.js

--- Node intro/ejs/index.js
+++ Node intro/ejs/index.js
@@ -11,6 +11,12 @@ app.get("/", (req, res)=>{
     res.render('home')
 })
 
+
+app.get('/r/:subreddit',(req,res)=>{
+    const {subreddit}= req.params;
+    res.render('subreddit',{subreddit})
+})
+
 app.get("/rand",(req, res)=>{
     const num= Math.floor(Math.random() * 10 )+1;
     res.render('random',{num})
@@ -20,5 +26,5 @@ app.get("/rand",(req, res)=>{
 
 
 app.listen(3000, ()=> {
-    console.log("Kaka");
+    console.log("listening on 3000");
 })
