6f3c7993e2e8af095a77f0c5ff04e3fd3d5ab40f
Sandro Pirveli <106743990+ItsKrampus@users.noreply.github.com>
2023-11-08 23:05:28 +0400

    Update index.js

--- Node intro/RESTDemo/index.js
+++ Node intro/RESTDemo/index.js
@@ -1,9 +1,64 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+const path=require('path')
+const{v4:uuid}=require('uuid')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
+
+
+let comments = [
+    { 
+        id:uuid(),
+        username: 'user1', 
+        comment: 'This is my first comment!' 
+    },
+    { 
+        id:uuid(),
+        username: 'user2', 
+        comment: 'Hello, everyone!' 
+    },
+    { 
+        id:uuid(),
+        username: 'user3', 
+        comment: 'Im excited about my new project.' 
+    },
+    { 
+        id:uuid(),
+        username: 'user4', 
+        comment: 'Just had a great day at the beach!' 
+    },
+    { 
+        id:uuid(),
+        username: 'user5', 
+        comment: 'Coding is so much fun!' 
+    }
+];
+
+
+app.get('/comments',(req,res)=>{
+    res.render('comments/index',{comments})
+})
+
+app.get('/comments/new', (req,res)=>{
+    res.render('comments/new')
+})
+
+app.post('/comments', (req,res)=>{
+    const {username, comment}=req.body;
+    comments.push({username,comment, id:uuid()})
+    res.redirect('/comments')
+})
+
+app.get('/comments/:id', (req,res)=>{
+    const {id}=req.params;
+    const comment= comments.find(c=>c.id===id)
+    res.render('comments/show',{comment})
+})
+
 
 app.get('/tacos',(req,res)=>{
     res.send("Get /tacos response")
@@ -17,3 +72,5 @@ app.post('/tacos',(req,res)=>{
 app.listen(3000, ()=>{
     console.log("on port 3000")
 })
+
+
